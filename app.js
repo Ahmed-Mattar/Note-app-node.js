4643121b5bd73df660da54e02208f6a56faff675
Ahmed Mattar <AceMattar@gmail.com>
2019-06-27 10:22:55 +0200

    feat:importing npm modules

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-// const add = require('./utils.js')
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
 // console.log(add(2,3));
 
 console.log(getNotes());
 
+console.log(validator.isEmail('@example.com'))
