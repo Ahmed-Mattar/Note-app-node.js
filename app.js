b52ca4912eaab58cc3d5d286ba70d1cbcd554e79
Ahmed Mattar <AceMattar@gmail.com>
2019-06-27 14:21:01 +0200

    feat:argument parsing with yargs

--- app.js
+++ app.js
@@ -1,12 +1,60 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2];
 
-console.log(process.argv)
+//customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+//create add command
+yargs.command({
+     command: 'add',
+     describe: 'Add a new note',
+     builder: {
+        title: {
+            describe: 'Note title',
+            demandOption:true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption:true,
+            type:'string'
+        }
+     },
+     handler: function (argv) {
+         console.log('Title: ' + argv.title)
+         console.log('Body: '+ argv.body)
+     }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note!')
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'list notes',
+    handler: function() {
+        console.log('list the note!')
+    }
+})
+
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function() {
+        console.log('read the note!')
+    }
+})
+
+// add, remove, read, list
+console.log(yargs.argv)
